651027709f0a9c1ed9e0646e267b4de28fc47e80
EPAM\Dhara_Basida <Dhara_Basida@epam.com>
2023-10-27 23:02:06 +0530

    updated e2e test case

--- demo-app/cypress/e2e/counter.cy.js
+++ demo-app/cypress/e2e/counter.cy.js
@@ -4,15 +4,15 @@ describe('Counter Component', () => {
       cy.visit('http://localhost:3000/'); // Update the URL as needed
   
       // Find the Counter Value element and assert its initial value
-      cy.get('h1').should('have.text', 'Counter Value: 0');
+      cy.get('h1').should('have.text', 'Counter Value: 10');
   
       // Click the Increment button and assert the updated value
       cy.get('button:contains(Increment)').click();
-      cy.get('h1').should('have.text', 'Counter Value: 1');
+      cy.get('h1').should('have.text', 'Counter Value: 11');
   
       // Click the Decrement button and assert the updated value
       cy.get('button:contains(Decrement)').click();
-      cy.get('h1').should('have.text', 'Counter Value: 0');
+      cy.get('h1').should('have.text', 'Counter Value: 10');
     });
   });
   
