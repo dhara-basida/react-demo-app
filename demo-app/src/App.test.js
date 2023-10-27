4c97817f8f994ae242c574094c93ec7f2b862382
EPAM\Dhara_Basida <Dhara_Basida@epam.com>
2023-10-27 11:34:08 +0530

    added test cases

--- demo-app/src/App.test.js
+++ demo-app/src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import './App.css';
+import Counter from './components/counter/Counter';
+import SearchForm from './components/search-form/SearchForm';
+import GenreSelect from './components/genre-select/GenreSelect';
+import React, { useState } from 'react';
 
-test('renders learn react link', () => {
+test('sample test case', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
