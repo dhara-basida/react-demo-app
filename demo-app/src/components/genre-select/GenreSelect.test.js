3c4550f14dfeb0206295d8e93be975fcc470ae8d
EPAM\Dhara_Basida <Dhara_Basida@epam.com>
2023-12-06 13:26:46 +0530

    resolved PR review comments

--- demo-app/src/components/genre-select/GenreSelect.test.js
+++ demo-app/src/components/genre-select/GenreSelect.test.js
@@ -6,7 +6,7 @@ import GenreSelect from './GenreSelect';
 describe('GenreSelect', () => {
     it('renders all genres passed in props', () => {
         const genres = ['All', 'Documentary', 'Comedy', 'Horror'];
-        const { getAllByRole } = render(<GenreSelect genres={genres} selectedGenre="" />);
+        const { getAllByRole } = render(<GenreSelect genres={genres} selectedGenre="" onSelect={() => { }} />);
 
         // Use getAllByText to find all buttons with genre text
         const genreButtons = getAllByRole('button');
@@ -21,8 +21,9 @@ describe('GenreSelect', () => {
     it('highlights the selected genre passed in props', () => {
         const genres = ['Action', 'Comedy', 'Drama', 'Science Fiction', 'Horror'];
         const selectedGenre = 'Comedy'; // Set the selected genre
+        const onSelect = jest.fn(); // Mock the onSelect function
         const { getByText } = render(
-            <GenreSelect genres={genres} selectedGenre={selectedGenre} />
+            <GenreSelect genres={genres} selectedGenre={selectedGenre} onSelect={onSelect} />
         );
 
         // Find the button with the selected genre text
@@ -30,5 +31,26 @@ describe('GenreSelect', () => {
 
         // Ensure that the selected button has the "selected-button" class
         expect(selectedButton).toHaveClass('selected-button');
+    });
+});
+
+//Test that after a click event on a genre button component calls "onChange" callback and passes correct genre in arguments
+describe('GenreSelect', () => {
+    it('calls the onSelect callback with the correct genre after a click event', () => {
+        const genres = ['Action', 'Comedy', 'Drama', 'Science Fiction', 'Horror'];
+        const selectedGenre = 'Comedy'; // Set the selected genre
+        const onSelect = jest.fn(); // Mock the onSelect function
+        const { getByText } = render(
+            <GenreSelect genres={genres} selectedGenre={selectedGenre} onSelect={onSelect} />
+        );
+
+        // Find the button with the selected genre text
+        const selectedButton = getByText('Drama');
+
+        // Simulate a click event on the selected button
+        fireEvent.click(selectedButton);
+
+        // Ensure that the onSelect callback was called with the correct genre
+        expect(onSelect).toHaveBeenCalledWith('Drama');
     });
 });
